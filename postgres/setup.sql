66b8335eb283548bb4676f5ca90a6511404589e8
Giovanni Astarita <6930874+obryan5598@users.noreply.github.com>
2022-01-10 21:36:07 +0100

    Update setup.sql

--- postgres/setup.sql
+++ postgres/setup.sql
@@ -7,4 +7,4 @@ CREATE TABLE BOOK (     ID SERIAL PRIMARY KEY,
                         PRICE DECIMAL (6,2) NOT NULL,
 			QUANTITY INTEGER NOT NULL);
 
-INSERT INTO BOOK(ID, ISBN, TITLE, AUTHOR, PRICE, QUANTITY) VALUES(nextval('hibernate_sequence'), '9780671820862', 'Captain America: Holocaust For Hire', 'Joseph Silva', 29.90, 20);
+INSERT INTO BOOK(ISBN, TITLE, AUTHOR, PRICE, QUANTITY) VALUES('9780671820862', 'Captain America: Holocaust For Hire', 'Joseph Silva', 29.90, 20);
